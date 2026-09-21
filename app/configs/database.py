213af2179a3509c94a9aa4cc983db27456cdc28f
Eldovyn <farras.pramudita@gmail.com>
2026-09-21 15:06:33 +0700

    fix(configs): add missing create_db_and_tables function in database config

--- app/configs/database.py
+++ app/configs/database.py
@@ -1,27 +1,43 @@
-from typing import Annotated, Generator
-from fastapi import Depends
-from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
 from app.configs.settings import settings
+from typing import Annotated, AsyncGenerator
+from fastapi import Depends
 
-connect_args = {}
-if settings.database_url.startswith("sqlite"):
-    connect_args = {"check_same_thread": False}
+SQLALCHEMY_DATABASE_URL = settings.database_url
 
-engine = create_engine(
-    settings.database_url,
-    echo=settings.debug,
-    connect_args=connect_args,
-)
+if SQLALCHEMY_DATABASE_URL.startswith("postgresql://"):
+    ASYNC_SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
+        "postgresql://", "postgresql+asyncpg://", 1
+    )
+elif SQLALCHEMY_DATABASE_URL.startswith("sqlite://"):
+    ASYNC_SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
+        "sqlite://", "sqlite+aiosqlite://", 1
+    )
+else:
+    ASYNC_SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL
+
+sync_engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL, future=True, echo=False)
+
+SyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=sync_engine)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 def create_db_and_tables() -> None:
-    SQLModel.metadata.create_all(engine)
+    import app.models  # noqa: F401
+    SQLModel.metadata.create_all(sync_engine)
 
 
-def get_session() -> Generator[Session, None, None]:
-    with Session(engine) as session:
-        yield session
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
 
 
-SessionDep = Annotated[Session, Depends(get_session)]
-
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
